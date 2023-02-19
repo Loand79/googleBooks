acc2542814fdbb823598c9582af87c05e7319611
Andrey Lozovik <tuoboy@gmail.com>
2023-02-19 18:41:54 +0300

    fetch response testing

--- src/App.js
+++ src/App.js
@@ -4,12 +4,21 @@ import { googleBooksAPI } from './services/googleBooksAPI';
 
 function App() {
 
-  const kek = googleBooksAPI.useGetGoogleBooksQuery('Негры')
-  console.log(kek);
+  const {data, isLoading, isSuccess} = googleBooksAPI.useGetGoogleBooksQuery('Негры')
+
 
   return (
     <div>
-
+      {isLoading
+          ?
+          <h1>Загрузка</h1>
+          :
+          <div>
+            {data.items.map(item =>
+              <p key={item.id}>{item.volumeInfo.title}</p>
+            )}
+          </div>
+      }
     </div>
   );
 }
